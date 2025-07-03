23c8e931f029ac6600dc699d17e35240e7daf521
ZarLiHnin <zarlehninn@gmail.com>
2025-07-03 16:06:38 +0900

    first commit

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,17 +1,8 @@
-// vitest.config.ts
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    environment: "jsdom",
     globals: true,
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"), // プロジェクトのsrcフォルダを@で参照
-    },
+    environment: "jsdom",
   },
 });
