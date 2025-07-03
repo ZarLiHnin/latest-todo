1bec3ddb988a1429d68cd2bb0a8d4a42ede29c76
ZarLiHnin <zarlehninn@gmail.com>
2025-07-03 16:32:39 +0900

    修正

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,11 +2,6 @@ import { defineConfig } from "vitest/config";
 import path from "path";
 
 export default defineConfig({
-  test: {
-    globals: true,
-    environment: "jsdom", // 👈 ここがポイント
-    include: ["**/*.test.ts"],
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
