301a8638c985cfdab3b014f7129834e96be7b0e1
ZarLiHnin <zarlehninn@gmail.com>
2025-07-03 17:32:15 +0900

    しゅうせい

--- lib/utils/formatDate.ts
+++ lib/utils/formatDate.ts
@@ -1,7 +1,9 @@
-export const formatDate = (dateString: string) => {
+export const formatDate = (dateString: string): string => {
   const date = new Date(dateString);
+
   return date
     .toLocaleString("ja-JP", {
+      timeZone: "Asia/Tokyo", // 明示的に日本時間にする
       year: "numeric",
       month: "2-digit",
       day: "2-digit",
